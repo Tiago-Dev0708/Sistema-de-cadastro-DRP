bb11621a61e743265e261bf8d54d888596a2ae67
tiago-goodai <tiago@wearegoodai.com>
2025-09-21 19:19:13 -0300

    feat:Adicionando a estruturação final a nivel de codigo

---
+++ api/utils/menu.py
@@ -0,0 +1,21 @@
+def menuCadastro(options):
+    match options:
+        case 1:
+            pass
+        case 2:
+           pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+           pass
+        case 6:
+            pass
+        case 7:
+            pass
+        case 8:
+           pass
+        case _:  
+            pass
+
